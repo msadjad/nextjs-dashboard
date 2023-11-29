6c16768f1123ee6f8c150acb325faeab77bd9364
Sadjad Fallah <m.sadjad.fallah@gmail.com>
2023-11-29 23:47:39 +0100

    Add pagination to invoice tables

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -13,12 +13,13 @@ export default function Search({ placeholder }: { placeholder: string }) {
     console.log(`Searching...${term}`);
 
     const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
     if (term) {
       params.set("query", term);
     } else {
-      params.delete("query")
+      params.delete("query");
     }
-    replace(`${pathname}?${params.toString()}`)
+    replace(`${pathname}?${params.toString()}`);
   }, 300);
 
   return (
@@ -32,7 +33,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
